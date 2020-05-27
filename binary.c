06f7eb4a3709de344323f4b97c817d5724156caa
test <knazar@localhost.localdomain>
2020-05-27 12:53:05 -0400

    refactoring and add search function

--- binary.c
+++ binary.c
@@ -3,8 +3,8 @@
 
 
 typedef struct node{
-		int data;
-		struct node* next;
+	int data;
+	struct node* next;
 }node_t;
 
 
@@ -12,69 +12,96 @@ node_t* init_list(int val);
 void push(node_t**,int);
 void begin_push(node_t**,int);
 void print_list(node_t*);
-
+void find_middle_value(node_t*,node_t*);
 int main()
 {
-		node_t* head;
-		begin_push(&head,2);
-		push(&head,3);
-		push(&head,4);
-		push(&head,5);
-		begin_push(&head,1);
-		print_list(head);
-		return 0;
+	node_t* head;
+	begin_push(&head,2);
+	push(&head,3);
+	push(&head,4);
+	push(&head,5);
+	begin_push(&head,1);
+	print_list(head);
+	return 0;
 }
 
 node_t* init_list(int val)
 {
-		node_t* current = malloc(sizeof(node_t));
-		if(current!=NULL)
-		{		current->data = val;
-				current->next = NULL;
-				return current;
-		}
+	node_t* current = malloc(sizeof(node_t));
+	if(current!=NULL)
+	{	
+		current->data = val;
+		current->next = NULL;
 		return current;
+	}
+
+	return current;
 }
 
 void push(node_t** head,int val)
 {
-		node_t* current = *head;
+	node_t* current = *head;
 
-		if(current == NULL)
-		{
-				*head =  init_list(val);
-				return;
-		}
+	if(current == NULL)
+	{
+		*head =  init_list(val);
+		return;
+	}
 
-		while(current->next != NULL)
-				current = current->next;
+	while(current->next != NULL)
+		current = current->next;
 
-		current->next = malloc(sizeof(node_t));
-		current->next->data = val;
-		current->next->next = NULL;
+	current->next = malloc(sizeof(node_t));
+	current->next->data = val;
+	current->next->next = NULL;
 }
 
 void begin_push(node_t** head, int val)
 {
-		node_t* tmp_node;
+	node_t* tmp_node;
 		
-		if(head == NULL)
-		{
-			*head = init_list(val);
-			return;
-		}
-		tmp_node = init_list(val);
-		tmp_node->next =*head;
-		*head = tmp_node;
+	if(head == NULL)
+	{
+		*head = init_list(val);
+		return;
+	}
+	tmp_node = init_list(val);
+	tmp_node->next =*head;
+	*head = tmp_node;
 }
 
 void print_list(node_t* head)
 {
-		node_t* current = head;
-		while(current!=NULL)
-		{
-				printf("%d->",current->data);
-				current=current->next;
-		}
-		printf("NULL\n");
+	node_t* current = head;
+	node_t* tail; 
+	while(current!=NULL)
+	{
+		printf("%d->",current->data);
+		current=current->next;
+	}
+	current = head;
+	printf("NULL\n");
+	while(current!=NULL)
+	{
+		printf("%d->",current);
+		if(current->next == NULL)
+			tail = current;
+		current=current->next;
+	}
+	printf("\n");
+
+	printf("from find_middle_value\n");
+	find_middle_value(head,tail);
+	
+}
+
+void find_middle_value(node_t *head,node_t* tail)
+{
+	node_t* current = head;
+	while(current!=tail)
+	{
+		printf("[%d] ",current->data);
+		current=current->next;
+	}
+	printf("\n");
 }
